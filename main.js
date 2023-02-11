48127cb4f95aac81adfda6fb489b77da952dccb5
akbar sydykov <sydykovakbar966@gmail.com>
2023-02-11 14:14:43 +0600

    LESSON-3 (DZ-3)

---
+++ main.js
@@ -0,0 +1,54 @@
+//DZ-1
+
+// function getMin(a ,b) {
+//     return (a < b) ? a : b;
+//
+// }
+// console.log(getMin(5, 8));
+// console.log(getMin(6, 10));
+
+
+
+
+//DZ-2
+
+// function popStringLength() {
+//     var str = prompt("Напиши что-нибудь:");
+//     console.log(" Длина твоей строки:" + str.length);
+//     return str.length;
+// }
+// popStringLength();
+
+
+
+
+//DZ-3
+
+// function calculate(num1, operator, num2) {
+//     if (operator === '+') {
+//         console.log(num1 + num2);
+//         return num1 + num2;
+//     } else if (operator === '-') {
+//         console.log(num1 - num2);
+//         return num1 - num2;
+//     } else if (operator === '*') {
+//         console.log(num1 * num2);
+//         return num1 * num2;
+//     } else if (operator === '/') {
+//       if  (num2 === 0) {
+//           console.log("На ноль не делится");
+//           return "На ноль не делится";
+//       }  else {
+//           console.log(num1 / num2);
+//           return num1 / num2;
+//       }
+//     } else {
+//         console.log("Оператор Неверный !");
+//         return "Оператор Неверный!";
+//     }
+// }
+//
+// calculate(33, '/', 8);
+// calculate();
+
+
